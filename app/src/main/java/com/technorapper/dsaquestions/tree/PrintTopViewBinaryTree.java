dafd47a208ea47a607f6f9ab617b723512ece692
Haneet Singh Chhabra <haneet555@gmail.com>
2022-09-09 00:44:33 +0530

    Initial commit

---
+++ app/src/main/java/com/technorapper/dsaquestions/tree/PrintTopViewBinaryTree.java
@@ -0,0 +1,130 @@
+package com.technorapper.dsaquestions.tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class PrintTopViewBinaryTree {
+
+    // Structure of binary tree
+    static class Node {
+        Node left;
+        Node right;
+        int data;
+    }
+
+    /*
+              1
+             / \
+            2   3
+             \
+              4
+               \
+                5
+                 \
+                  6
+        Top view of the above binary tree is
+        2 1 3 6
+    */
+    static class pair {
+        Node node;
+        int hd;
+
+        pair() {
+        }
+
+        pair(Node node, int hd) {
+            this.node = node;
+            this.hd = hd;
+        }
+    }
+
+    // function to create a new node
+    static Node newNode(int key) {
+        Node node = new Node();
+        node.left = node.right = null;
+        node.data = key;
+        return node;
+    }
+
+    // function should print the topView of
+    // the binary tree
+    static void topView(Node root) {
+        // queue for holding nodes and their horizontal
+        // distance from the root node
+        Queue<pair> q = new LinkedList<>();
+
+        // pushing root node with distance 0
+        q.add(new pair(root, 0));
+
+        // hd is current node's horizontal distance from
+        // root node l is current left min horizontal
+        // distance (or max in magnitude) so far from the
+        // root node r is current right max horizontal
+        // distance so far from the root node
+
+        int hd = 0, l = 0, r = 0;
+
+        // stack is for holding left node's data because
+        // they will appear in reverse order that is why
+        // using stack
+
+        Stack<Integer> left = new Stack<>();
+
+        // ArrayList is for holding right node's data
+        ArrayList<Integer> right = new ArrayList<>();
+        Node node = null;
+
+        while (!q.isEmpty()) {
+            node = q.peek().node;
+            hd = q.peek().hd;
+
+            if (hd < l) {
+                left.push(node.data);
+                l = hd;
+            }
+
+            if (hd > r) {
+                right.add(node.data);
+                r = hd;
+            }
+
+            if (node.left != null) {
+                q.add(new pair(node.left, hd - 1));
+            }
+            if (node.right != null) {
+                q.add(new pair(node.right, hd + 1));
+            }
+
+            q.poll();
+        }
+
+        // printing the left node's data in reverse order
+        while (!left.isEmpty()) {
+            System.out.print(left.peek() + " ");
+            left.pop();
+        }
+
+        // then printing the root node's data
+        System.out.print(root.data + " ");
+
+        // finally printing the right node's data
+        for (int d : right) {
+            System.out.print(d + " ");
+        }
+    }
+
+    // Driver Code
+    public static void main(String args[]) {
+        Node root = newNode(1);
+        root.left = newNode(2);
+        root.right = newNode(3);
+        root.left.right = newNode(4);
+        root.left.right.right = newNode(5);
+        root.left.right.right.right = newNode(6);
+        System.out.println("Following are nodes in"
+                + " top view of Binary Tree");
+        topView(root);
+    }
+}
