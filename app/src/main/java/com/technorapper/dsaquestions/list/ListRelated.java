d9806b0e8e5570dbceb197e837154a206f93d7f3
Haneet Singh Chhabra <haneet555@gmail.com>
2022-10-20 22:52:13 +0530

    list

--- app/src/main/java/com/technorapper/dsaquestions/list/ListRelated.java
+++ app/src/main/java/com/technorapper/dsaquestions/list/ListRelated.java
@@ -14,6 +14,19 @@ public class ListRelated {
         integerList.set(0,99); // update value of 0 index
 
         System.out.println(integerList);
+        test();
+    }
+
+    static void  test(){
+        int i =0;
+        int sum =0;
+        while(i<100){
+            sum=sum+1;
+            sum=i+sum;
+            i+=1;
+        }
+
+        System.out.println(sum);
     }
 
 }
