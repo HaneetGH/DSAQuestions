aec6065b807d822d2cc39545aa60fe9764158fbc
Haneet Singh Chhabra <haneet555@gmail.com>
2022-10-30 15:09:18 +0530

    list

--- app/src/main/java/com/technorapper/dsaquestions/list/ListRelated.java
+++ app/src/main/java/com/technorapper/dsaquestions/list/ListRelated.java
@@ -5,16 +5,17 @@ import java.util.List;
 
 public class ListRelated {
 
+
     public static void main(String[] args) {
         List<Integer> integerList = new ArrayList<>();
         for (int i = 0; i < 5; i++)
             integerList.add(i);
         integerList.add(0,88); // Add item at zero and lift other one step up
         integerList.set(0,99); // update value of 0 index
-
         System.out.println(integerList);
         test();
     }
+
     static void  test(){
         int i =0;
         int sum =0;
